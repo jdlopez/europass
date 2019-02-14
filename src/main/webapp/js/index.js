1c5c6fdbd52c8d64a7bb50fe187ffe8b443dcd40
jesusdavid.lopez <jesusdavid.lopez@gmail.com>
2019-02-15 00:12:23 +0100

    new button dialog

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -29,7 +29,104 @@ const Home = {
 }
 
 const HRJobs = {
-  template: '<div>foo <a href="/hr/job:1">detalle 1</a></div>'
+  template: `
+    <div>
+    
+    <b-table striped hover :busy.sync="isBusy" :items="jobsProvider" :fields="fields"></b-table>
+    
+    <b-button v-b-modal.modal1>New</b-button>
+    
+    <!-- Modal Component -->
+    <b-modal id="modal1" title="Job detail">
+     <b-form @submit="onSubmit" @reset="onReset" >
+      <b-form-group
+        id="exampleInputGroup1"
+        label="Email address:"
+        label-for="exampleInput1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="exampleInput1"
+          type="email"
+          v-model="form.email"
+          required
+          placeholder="Enter email" />
+      </b-form-group>
+
+      <b-form-group id="exampleInputGroup2" label="Your Name:" label-for="exampleInput2">
+        <b-form-input
+          id="exampleInput2"
+          type="text"
+          v-model="form.name"
+          required
+          placeholder="Enter name" />
+      </b-form-group>
+
+      <b-form-group id="exampleInputGroup3" label="Food:" label-for="exampleInput3">
+        <b-form-select id="exampleInput3" :options="foods" required v-model="form.food" />
+      </b-form-group>
+
+      <b-form-group id="exampleGroup4">
+        <b-form-checkbox-group v-model="form.checked" id="exampleChecks">
+          <b-form-checkbox value="me">Check me out</b-form-checkbox>
+          <b-form-checkbox value="that">Check that out</b-form-checkbox>
+        </b-form-checkbox-group>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    </b-modal>
+
+    </div>
+  `,
+    data() {
+      return {
+          isBusy: false,
+          fields: [
+              "id", "name", "locationCountry", "locationCity",
+              {key: "openToPublic", label: "public"}, "status", "creationDate"
+          ],
+          form: {
+              email: '',
+              name: '',
+              food: null,
+              checked: []
+          },
+          foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn']
+      }
+    },
+    methods: {
+        jobsProvider(ctx) {
+            let promise = axios.get('/hr/jobs')
+            return promise.then((data) => {
+                const items = data.data;
+            return(items)
+        }).catch(error => {
+                console.log('error reading jobs: ' + data);
+            return []
+        })
+        },
+        onSubmit(evt) {
+            evt.preventDefault()
+            alert(JSON.stringify(this.form))
+        },
+        onReset(evt) {
+            evt.preventDefault()
+            /* Reset our form values */
+            this.form.email = ''
+            this.form.name = ''
+            this.form.food = null
+            this.form.checked = []
+            /* Trick to reset/clear native browser form validation state */
+            this.show = false
+            this.$nextTick(() => {
+                this.show = true
+        })
+        }
+    }
+
+
 }
 
 const HRJobDetail = {
